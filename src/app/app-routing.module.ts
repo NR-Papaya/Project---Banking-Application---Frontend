e475ad273ff58005bf72e441e70bed63d68b42b2
Nahia Riviera <n.riviera.dev@gmail.com>
2022-11-03 17:51:03 -0500

    styling work and background added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageHomeComponent } from './components/pages/page-home/page-home.component';
+import { PageUserHomeComponent } from './components/pages/page-user-home/page-user-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: PageHomeComponent
+  },
+  {
+    path:'userhome',
+    component: PageUserHomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

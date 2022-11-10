8832b30755af50cc69c0ef91224cb89c5a67992d
Nahia Riviera <n.riviera.dev@gmail.com>
2022-11-09 21:29:41 -0600

    cors issue resolved

--- src/app/services/database-connection.service.ts
+++ src/app/services/database-connection.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UserModel } from '../models/UserModel';
 import { LoginCredentialModel } from '../models/loginCredentialModel';
@@ -11,7 +11,18 @@ export class DatabaseConnectionService {
   constructor(private httpClient: HttpClient) {}
 
   attemptLogin(credentials: LoginCredentialModel): Observable<any> {
+    const httpOptions = { headers: new HttpHeaders({
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type':'application/json'
+    }) };
+
+    
+      console.log(httpOptions)
     let body = JSON.stringify(credentials);
-    return this.httpClient.post<any>('http://localhost:8080/logIn', body);
+    return this.httpClient.post<any>(
+      'http://localhost:8080/logIn',
+      body,
+      httpOptions
+    );
   }
 }

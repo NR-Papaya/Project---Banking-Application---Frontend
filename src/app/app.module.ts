8419da96cf4899b295d9da17d99572342ab70b12
Nahia Riviera <n.riviera.dev@gmail.com>
2022-11-18 12:19:43 -0600

    rebuilding transfers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,18 +19,17 @@ import { AccountsDisplayComponent } from './components/pages/page-user-home/user
 import { AccountCardsComponent } from './components/pages/page-user-home/user-accounts-view/accounts-display/account-cards/account-cards.component';
 import { AccountBalanceComponent } from './components/pages/page-user-home/user-accounts-view/accounts-display/account-balance/account-balance.component';
 import { TransactionsTableComponent } from './components/pages/page-user-home/user-accounts-view/accounts-display/transactions-table/transactions-table.component';
-import { AddAccountPopupComponent } from './components/pages/page-user-home/user-accounts-view/accounts-display/add-account-popup/add-account-popup.component'
+import { AddAccountPopupComponent } from './components/pages/page-user-home/user-accounts-view/accounts-display/add-account-popup/add-account-popup.component';
 
-
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatCardModule } from "@angular/material/card";
-import { MatButtonModule } from "@angular/material/button";
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select'
+import { MatSelectModule } from '@angular/material/select';
 
-import { BrowserAnimationsModule  } from '@angular/platform-browser/animations'
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccountTransferComponent } from './components/pages/page-user-home/user-accounts-view/accounts-display/account-transfer/account-transfer.component';
 
 @NgModule({
   declarations: [
@@ -49,12 +48,12 @@ import { BrowserAnimationsModule  } from '@angular/platform-browser/animations'
     AccountBalanceComponent,
     TransactionsTableComponent,
     AddAccountPopupComponent,
-
-   
+    AccountTransferComponent,
   ],
-  imports: [BrowserModule, 
-    AppRoutingModule, 
-    HttpClientModule, 
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
     FormsModule,
     MatDialogModule,
     MatCardModule,
@@ -62,8 +61,7 @@ import { BrowserAnimationsModule  } from '@angular/platform-browser/animations'
     MatFormFieldModule,
     MatInputModule,
     BrowserAnimationsModule,
-    MatSelectModule
-     
+    MatSelectModule,
   ],
   providers: [DatabaseConnectionService],
   bootstrap: [AppComponent],
